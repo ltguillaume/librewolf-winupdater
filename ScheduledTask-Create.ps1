f97849216e9f04ed5263f1de7d010bfd1de00683
ltguillaume <ltguillaume@users.noreply.github.com>
2023-09-30 21:31:49 +0200

    ScheduledTask-Create: Fix user for some cases (Codeberg #28, thx @NightFuture!)

--- ScheduledTask-Create.ps1
+++ ScheduledTask-Create.ps1
@@ -4,9 +4,10 @@ $Host.UI.RawUI.WindowTitle = $Title
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   Write-Output "Requesting administrator privileges"
-  $User = [Environment]::UserName
+  $User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+  $UserName = [Environment]::UserName
   $Script = $MyInvocation.MyCommand.Path
-  Start-Process powershell.exe -Verb RunAs "-ExecutionPolicy RemoteSigned -File `"$PSCommandPath`" `"${User}`""
+  Start-Process powershell.exe -Verb RunAs "-ExecutionPolicy RemoteSigned -File `"$PSCommandPath`" `"${User}`" `"${UserName}`""
   Exit
 }
 
@@ -15,8 +16,9 @@ $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoi
 $4Hours   = New-ScheduledTaskTrigger -Once -At (Get-Date -Minute 0 -Second 0).AddHours(1) -RepetitionInterval (New-TimeSpan -Hours 4)
 $AtLogon  = New-ScheduledTaskTrigger -AtLogOn
 $AtLogon.Delay = 'PT1M'
-$User     = If ($Args[0]) {$Args[0]} Else {[Environment]::UserName}
+$User     = If ($Args[0]) {$Args[0]} Else {[System.Security.Principal.WindowsIdentity]::GetCurrent().Name}
+$UserName = If ($Args[1]) {$Args[1]} Else {[Environment]::UserName}
 
-Register-ScheduledTask -TaskName "$Title ($User)" -Action $Action -Settings $Settings -Trigger $4Hours,$AtLogon -User $User -RunLevel Highest -Force
+Register-ScheduledTask -TaskName "$Title ($UserName)" -Action $Action -Settings $Settings -Trigger $4Hours,$AtLogon -User $User -RunLevel Highest -Force
 Write-Output "Done. Press any key to close this window."
 [Console]::ReadKey()
--- ScheduledTask-Remove.ps1
+++ ScheduledTask-Remove.ps1
@@ -4,14 +4,14 @@ $Host.UI.RawUI.WindowTitle = $Title
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   Write-Output "Requesting administrator privileges"
-  $User = [Environment]::UserName
+  $UserName = [Environment]::UserName
   $Script = $MyInvocation.MyCommand.Path
-  Start-Process powershell.exe -Verb RunAs "-ExecutionPolicy RemoteSigned -File `"$PSCommandPath`" `"${User}`""
+  Start-Process powershell.exe -Verb RunAs "-ExecutionPolicy RemoteSigned -File `"$PSCommandPath`" `"${UserName}`""
   Exit
 }
 
-$User = If ($Args[0]) {$Args[0]} Else {[Environment]::UserName}
+$UserName = If ($Args[0]) {$Args[0]} Else {[Environment]::UserName}
 
-Unregister-ScheduledTask -TaskName "$Title ($User)" -Confirm:$False
+Unregister-ScheduledTask -TaskName "$Title ($UserName)" -Confirm:$False
 Write-Output "Done. Press any key to close this window."
 [Console]::ReadKey()
