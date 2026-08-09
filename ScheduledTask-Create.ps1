9fdd6aa052069b52648d484fd74445f68668a163
ltguillaume <ltguillaume@noreply.codeberg.org>
2026-08-09 14:46:00 +0200

    Refactor and improve wording in scheduled task scripts

--- ScheduledTask-Create.ps1
+++ ScheduledTask-Create.ps1
@@ -1,9 +1,12 @@
-$Updater = "LibreWolf WinUpdater"
-$Host.UI.RawUI.WindowTitle = "PowerShell: Create $Updater scheduled task"
+$Browser = "LibreWolf"
+$Hours   = 4
+
+$Updater = "$Browser WinUpdater"
+$Host.UI.RawUI.WindowTitle = "$Updater Task Scheduler"
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-  Write-Output "`nTo create the task, please allow PowerShell to make changes to your device.`n`nPress any key to continue..."
+  Write-Output "`nTo enable automatic updates, please allow PowerShell to make changes to your device.`n`nPress any key to continue..."
   [Console]::ReadKey()
   $User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
   $UserName = [Environment]::UserName
@@ -13,14 +16,15 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 $UserName = If ($Args[1]) {$Args[1]} Else {[Environment]::UserName}
-$Action   = New-ScheduledTaskAction -Execute "LibreWolf-WinUpdater.exe" -Argument "/Scheduled" -WorkingDirectory "$PSScriptRoot"
+$TaskName = "$Updater ($UserName)"
+$Action   = New-ScheduledTaskAction -Execute "$Browser-WinUpdater.exe" -Argument "/Scheduled" -WorkingDirectory "$PSScriptRoot"
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DisallowHardTerminate -RunOnlyIfNetworkAvailable -StartWhenAvailable
-$4Hours   = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 4)
+$Interval = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours $Hours)
 $AtLogon  = New-ScheduledTaskTrigger -AtLogOn
 $AtLogon.Delay = 'PT1M'
 $User     = If ($Args[0]) {$Args[0]} Else {[System.Security.Principal.WindowsIdentity]::GetCurrent().Name}
 
-Register-ScheduledTask -TaskName "$Updater ($UserName)" -Action $Action -Settings $Settings -Trigger $4Hours,$AtLogon -User $User -RunLevel Highest -Force
-If ($?) { Write-Output "`nTask created." }
+Register-ScheduledTask -TaskName $TaskName -Action $Action -Settings $Settings -Trigger $Interval,$AtLogon -User $User -RunLevel Highest -Force
+If ($?) { Write-Output "`nTask for automatic updates created successfully." }
 Write-Output "`nPress any key to continue..."
 [Console]::ReadKey()
--- ScheduledTask-Remove.ps1
+++ ScheduledTask-Remove.ps1
@@ -1,9 +1,11 @@
-$Updater = "LibreWolf WinUpdater"
-$Host.UI.RawUI.WindowTitle = "PowerShell: Remove $Updater scheduled task"
+$Browser = "LibreWolf"
+
+$Updater = "$Browser WinUpdater"
+$Host.UI.RawUI.WindowTitle = "$Updater Task Scheduler"
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-  Write-Output "`nTo remove the task, please allow PowerShell to make changes to your device.`n`nPress any key to continue..."
+  Write-Output "`nTo disable automatic updates, please allow PowerShell to make changes to your device.`n`nPress any key to continue..."
   [Console]::ReadKey()
   $UserName = [Environment]::UserName
   $Script = $MyInvocation.MyCommand.Path
@@ -12,9 +14,10 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 $UserName = If ($Args[0]) {$Args[0]} Else {[Environment]::UserName}
+$TaskName = "$Updater ($UserName)"
 
-Unregister-ScheduledTask -TaskName "$Updater ($UserName)" -Confirm:$False
+Unregister-ScheduledTask -TaskName $TaskName -Confirm:$False
 
-If ($?) { Write-Output "`nTask removed." }
+If ($?) { Write-Output "`n$TaskName`n`nTask for automatic updates removed successfully." }
 Write-Output "`nPress any key to continue..."
 [Console]::ReadKey()
